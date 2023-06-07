05c164dc1fd5d1644d93e1d330c1ecb95f8f3ec1
tbrown034 <tbrown034@gmail.com>
2023-06-07 15:10:40 -0500

    feat: handle submit and handle change hooked up

--- app/page.js
+++ app/page.js
@@ -1,4 +1,20 @@
+"use client";
+import { useState } from "react";
+
 export default function Home() {
+  const [inputQuestion, setInputQuestion] = useState("");
+  const [question, setQuestion] = useState("");
+  const [returnQuestion, setReturnQuestion] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("question", question);
+  };
+
+  const handleChange = (e) => {
+    setQuestion(e.target.value);
+  };
+
   return (
     <main className="flex flex-col items-center justify-center min-h-screen gap-4 px-2 text-white bg-cyan-700">
       <h1 className="text-4xl font-bold">Dental GPT</h1>
@@ -7,14 +23,18 @@ export default function Home() {
         quick answers to any of you dental needs.
       </p>
 
-      <form className="flex flex-col gap-2 p-4 border-4 border-cyan-500">
+      <form
+        className="flex flex-col gap-2 p-4 border-4 border-cyan-500"
+        onSubmit={handleSubmit}
+      >
         <div className="flex flex-col items-center gap-2">
           <label>Your Question:</label>
           <textarea
-            resize
             rows={4}
             placeholder="Your question here ..."
-            className="px-2 text-black resize-none md:w-96 w-72 bg-lime-50"
+            className="px-2 text-black md:w-96 w-72 bg-lime-50"
+            value={question}
+            onChange={handleChange}
           ></textarea>
         </div>
         <button className="text-sm bg-cyan-600 hover:bg-cyan-500">
