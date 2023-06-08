9e361436dcfe449585ebe1afcec4b64b40a4d670
tbrown034 <tbrown034@gmail.com>
2023-06-07 21:29:40 -0500

    feat: many UI changes

--- app/page.js
+++ app/page.js
@@ -1,32 +1,28 @@
 "use client";
 import { motion } from "framer-motion";
-
 import { useState } from "react";
 import { getAIResponse } from "./utils/gtpAPI.jsx";
 
 export default function Home() {
   const [question, setQuestion] = useState("");
   const [isSubmitted, setIsSubmitted] = useState(false);
-  const [aiResponse, setAIResponse] = useState(""); // Add a new state variable for the AI response
+  const [aiResponse, setAIResponse] = useState("");
 
   const handleChange = (e) => {
     setQuestion(e.target.value);
-    console.log("target", e.target.value);
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log("submitted question", question);
-    const response = await getAIResponse(question); // Get the AI response
-    setAIResponse(response); // Set the AI response
-    setIsSubmitted(true); // Set isSubmitted to true when the form is submitted
+    const response = await getAIResponse(question);
+    setAIResponse(response);
+    setIsSubmitted(true);
   };
 
   const handleKeyPress = (e) => {
     if (e.key === "Enter" && !e.shiftKey) {
-      // Check if the Enter key was pressed without the Shift key
-      e.preventDefault(); // Prevent the default behavior (creating a new line in the textarea)
-      handleSubmit(e); // Submit the form
+      e.preventDefault();
+      handleSubmit(e);
     }
   };
 
@@ -36,34 +32,34 @@ export default function Home() {
   };
 
   return (
-    <main className="flex flex-col min-h-screen px-6 text-white bg-teal-900 ">
-      <section className="flex flex-col justify-center min-h-screen text-white bg-teal-900 gap-14 ">
+    <main className="flex flex-col p-6 text-white bg-gradient-to-r from-teal-800 to-sky-800">
+      <section className="flex flex-col justify-center min-h-screen gap-14">
         <motion.div
-          className="flex flex-col gap-2 "
-          initial={{ opacity: 0, y: -50 }}
+          className="flex flex-col gap-2"
+          initial={{ opacity: 0, y: -100 }}
           animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 1 }}
+          transition={{ duration: 2 }}
         >
-          <h1 className="text-5xl font-extrabold ">Dental GPT</h1>
-          <h3 className="font-mono text-xl ">
-            An <span className="text-teal-200">AI-powered</span> chatbox
-            designed to answer all your{" "}
-            <span className="text-teal-200">dental questions</span>
+          <h1 className="text-5xl font-extrabold leading-relaxed tracking-wider text-teal-200 md:text-8xl">
+            Dental GPT
+          </h1>
+          <h3 className="font-mono text-lg text-teal-300 sm:text-2xl">
+            An <span className="text-white">AI-powered</span> chatbox designed
+            to answer all your <span className="text-white">dental</span>{" "}
+            questions
           </h3>
         </motion.div>
+
         {!isSubmitted && (
-          <form
-            onSubmit={handleSubmit}
-            className="flex flex-col justify-center text-2xl"
-          >
+          <form onSubmit={handleSubmit} className="flex flex-col text-xl">
             <textarea
               placeholder="Ask your dental question here ..."
-              className="w-full h-40 p-2 text-teal-800 bg-teal-50"
+              className="w-full h-40 p-4 text-3xl font-semibold text-teal-600 bg-teal-100 rounded-lg shadow-md focus:outline-none focus:ring-2 focus:ring-teal-300"
               value={question}
               onChange={handleChange}
               onKeyPress={handleKeyPress}
             />
-            <button className="py-2 mt-8 bg-teal-700 rounded-lg hover:bg-teal-50 hover:text-teal-700">
+            <button className="py-2 mt-8 transition-all duration-200 ease-in-out bg-teal-700 rounded-lg shadow-lg text-teal-50 hover:bg-teal-600 focus:outline-none focus:ring-2 focus:ring-teal-200">
               Send Question
             </button>
           </form>
@@ -71,22 +67,22 @@ export default function Home() {
 
         {isSubmitted && (
           <motion.div
-            className="flex flex-col gap-4 text-2xl"
-            initial={{ opacity: 0, y: 50 }}
+            className="flex flex-col gap-4 text-xl"
+            initial={{ opacity: 0, y: -100 }}
             animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.5 }}
+            transition={{ duration: 2 }}
           >
-            <div className="p-2 bg-teal-700 rounded-lg">
-              <p className="font-bold text-teal-50">You</p>
-              <p className="text-white">{question}</p>
+            <div className="p-4 text-teal-100 bg-teal-700 rounded-lg shadow-lg">
+              <p className="font-bold">You</p>
+              <p>{question}</p>
             </div>
-            <div className="flex flex-col items-end p-2 bg-teal-500 rounded-lg">
-              <p className="font-bold text-teal-50">Dental GPT</p>
-              <p className="text-white">{aiResponse}</p>
+            <div className="flex flex-col items-end p-4 bg-teal-600 rounded-lg shadow-lg text-teal-50">
+              <p className="font-bold">Dental GPT</p>
+              <p>{aiResponse}</p>
             </div>
             <button
               onClick={handleReset}
-              className="w-full py-2 mt-8 bg-teal-700 rounded-lg hover:bg-teal-50 hover:text-teal-700"
+              className="w-full py-2 mt-8 transition-all duration-200 ease-in-out bg-teal-700 rounded-lg shadow-lg text-teal-50 hover:bg-teal-600 focus:outline-none focus:ring-2 focus:ring-teal-200"
             >
               Ask Another Question
             </button>
@@ -94,10 +90,10 @@ export default function Home() {
         )}
       </section>
       <div>
-        <p className="py-2 text-sm border-t-2 border-white">
+        <p className="py-2 mt-8 text-xs text-teal-400 border-t-2 border-white">
           This website was created by Trevor Brown using a tech stack that
-          includes Node.js, Next.js., tailwind and the GPT-3 API from Open AI.
-          Thanks for visiting!
+          includes Node.js, Next.js, Tailwind CSS, and the GPT-3 API from Open
+          AI. Thanks for visiting!
         </p>
       </div>
     </main>
